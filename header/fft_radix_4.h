2fb16e858f1a1f31e331ef2522be4d1ce2274d96
AdityaP1502 <i.m.aditya015@gmail.com>
2023-08-03 10:50:42 +0700

    Change function parameter order for consistency

--- header/fft_radix_4.h
+++ header/fft_radix_4.h
@@ -9,13 +9,13 @@ FFTLIBRARY_API fft_bins* FFTLIBRARY_CALL fft_radix_4_iterative_static_n(bins for
 /*  Calculate IFFT of Xk using radix 4 fft. Xk is ordered and the result is ordered
     The order of calculation will be using the NR mode.  
 */
-FFTLIBRARY_API fft_bins* FFTLIBRARY_CALL ifft_radix_4_iterative_static_n(bins xk, bins backward_twid_factor, int fft_size, int length);
+FFTLIBRARY_API fft_bins* FFTLIBRARY_CALL ifft_radix_4_iterative_static_n(bins backward_twid_factor, int fft_size, bins xk, int length);
 
 /*  Calculate IFFT of xn using radix 4 fft. Xk is ordered and the result is ordered.
     The order of calculation will be using the NR mode.  
     The result use the assumption that Xk is symmetric, therefore the imaginary part of the answer is discarded
 */
-FFTLIBRARY_API ifft_symmetric_bins* FFTLIBRARY_CALL ifft_radix_4_iterative_symmetric_static_n(bins Xk, bins backward_twid_factor, int fft_size, int length);
+FFTLIBRARY_API ifft_symmetric_bins* FFTLIBRARY_CALL ifft_radix_4_iterative_symmetric_static_n(bins backward_twid_factor, int fft_size, bins xk, int length);
 
 /*  Calculate FFT of xn using radix 4 fft. Xn is ordered and the result is ordered
     The order of calculation will be using the NR mode.  
