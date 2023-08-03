ccef452a57dad21697280b3581fb69a5905972cb
AdityaP1502 <i.m.aditya015@gmail.com>
2023-08-03 10:50:02 +0700

    Moved static util function to fft.c

--- src/iterative_fft.c
+++ src/iterative_fft.c
@@ -235,15 +235,6 @@ fft_bins *FFTLIBRARY_CALL fft_iterative(double *xn, int length, char *order)
     return res->fft_bins ? res : NULL;
 }
 
-static void FFTLIBRARY_CALL normalize_ifft(bins xk, int length)
-{
-    for (int i = 0; i < length; i++)
-    {
-        xk[i]->real = xk[i]->real / length;
-        xk[i]->imag = xk[i]->imag / length;
-    }
-}
-
 fft_bins *FFTLIBRARY_CALL ifft_iterative(bins xk, int length, char *order)
 {
     int pad_length, input_length;
@@ -298,70 +289,6 @@ ifft_symmetric_bins *FFTLIBRARY_CALL ifft_iterative_symmetric(bins xk, int lengt
     free(output_ifft);
 
     return res;
-}
-
-static bins FFTLIBRARY_CALL combined_two_real_input(double *xn_1, double *xn_2, int max_length, int min_length)
-{
-    bins combined_bin = malloc(max_length * sizeof(complex_number *));
-
-    // make X[k] = xn_1[k] + jxn_2[k]
-    for (int i = 0; i < min_length; i++)
-    {
-        combined_bin[i] = create_complex_number(xn_1[i], xn_2[i]);
-    }
-
-    for (int i = min_length; i < max_length; i++)
-    {
-        combined_bin[i] = create_complex_number(xn_1[i], 0);
-    }
-
-    return combined_bin;
-}
-
-static void FFTLIBRARY_CALL seperate_combined_output(bins combined_output, int length, fft_bins **dest)
-{
-    fft_bins *result_1;
-    fft_bins *result_2;
-
-    bins fr;
-    bins gr;
-
-    double real, imag;
-
-    fr = malloc(length * sizeof(complex_number *));
-    gr = malloc(length * sizeof(complex_number *));
-
-    result_1 = malloc(sizeof(fft_bins));
-    result_2 = malloc(sizeof(fft_bins));
-
-    fr[0] = create_complex_number(combined_output[0]->real, 0);
-    gr[0] = create_complex_number(combined_output[0]->imag, 0);
-
-    for (int i = 1; i < length; i++)
-    {
-        // fr = 0.5 (xr + x*(N - r))
-        real = 0.5 * (combined_output[i]->real + combined_output[length - i]->real);
-        imag = 0.5 * (combined_output[i]->imag - combined_output[length - i]->imag);
-
-        fr[i] = create_complex_number(real, imag);
-
-        // gr = 0.5j (x*(N - r) - x(r) -> 0.5(jA* - jB)
-        // gr = 0.5 (jA.real + A.imag - (-B.imag + jB.real)) -> 1/2(A.imag + B.imag) + 1/2j(A.real - B.real)
-
-        real = 0.5 * (combined_output[length - i]->imag + combined_output[i]->imag);
-        imag = 0.5 * (combined_output[length - i]->real - combined_output[i]->real);
-
-        gr[i] = create_complex_number(real, imag);
-    }
-
-    result_1->fft_bins = fr;
-    result_1->length = length;
-
-    result_2->fft_bins = gr;
-    result_2->length = length;
-
-    dest[0] = result_1;
-    dest[1] = result_2;
 }
 
 fft_bins **FFTLIBRARY_CALL fft_double_real(double *xn_1, double *xn_2, int length_1, int length_2, char *order)
@@ -483,14 +410,6 @@ fft_bins *FFTLIBRARY_CALL ifft_iterative_static_n(bins xk, bins backward_twid_fa
 {
     int pad_length, input_length;
     bins input_array, output_array;
-
-    // printf("%d\n", length);
-    // for (int i = 0; i < length; i++)
-    // {
-    //     str = complex_number_to_string(xk[i]);
-    //     printf("%s\n", str);
-    //     free(str);
-    // }
 
     output_array = NULL;
     input_array = xk;
