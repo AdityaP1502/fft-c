1359a33c2faf186332504857ddce29b901e19ef4
AdityaP1502 <i.m.aditya015@gmail.com>
2023-08-20 18:50:58 +0700

    Refactor precompute twid factor function

--- src/simd/fft_simd.c
+++ src/simd/fft_simd.c
@@ -1,6 +1,65 @@
 #include <string.h>
 #include "../../header/simd/complex_simd.h"
 
+static void twid_calc_routine(complex* twiddle_factors, int l, int halfl, int quarterl, int eightl, int backward)
+{
+  double theta;
+
+  double magic_factor_1;
+  double magic_factor_2;
+
+  int halfl;
+  int eightl;
+  int quarterl;
+
+  theta = backward ? (M_PI * 2) / l : -(M_PI * 2) / l;
+  magic_factor_1 = 1 - 2 * pow(sin(theta / 2), 2); // C
+  magic_factor_2 = sin(theta);  
+
+  for (int i = 1; i <= eightl; i++)
+    {
+      // using singleteon
+      // cos ((r + 1)theta) = C * cos(rtheta) - S * sin(rtheta)
+      // sin((r + 1)theta) = S * cos(rtheta) + C * sin(rtheta)
+
+      twiddle_factors->real[i] = magic_factor_1 * twiddle_factors->real[i - 1] - magic_factor_2 * twiddle_factors->imag[i - 1];
+      twiddle_factors->imag[i] = magic_factor_2 * twiddle_factors->real[i - 1] + magic_factor_1 * twiddle_factors->imag[i - 1];
+    }
+
+    for (int i = 1; i < eightl; i++)
+    {
+      // cos((N/8 + k) * theta) = sin((N/8 - k) * theta)
+      // sin((N/8 + k) * theta) = cos((N/8 - k) * theta)
+
+      twiddle_factors->real[eightl + i] = backward ? twiddle_factors->imag[eightl - i] : -twiddle_factors->imag[eightl - i];
+      twiddle_factors->imag[eightl + i] = backward ? twiddle_factors->real[eightl - i] : -twiddle_factors->real[eightl - i];
+    }
+
+    // calculate the other N / 4 points in second quadrand
+    for (int i = 0; i <= quarterl; i++)
+    {
+      // cos((N/4 + k) * theta) = -sin(k * theta)
+      // sin((N/4 + k) * theta) = cos(k * theta)
+
+      twiddle_factors->real[quarterl + i] = backward ? -twiddle_factors->imag[i] : twiddle_factors->imag[i];
+      twiddle_factors->imag[quarterl + i] = backward ? twiddle_factors->real[i] : -twiddle_factors->real[i];
+    }
+}
+
+static void twid_calc_routine_r4(complex* twiddle_factors, int l, int halfl, int quarterl, int eightl, int backward) 
+{
+  twid_calc_routine(twiddle_factors, l, halfl, quarterl, eightl, backward);
+
+  // Extra N/4 
+  // calculate the other N / 4 points in third quadrand
+  for (int i = 1; i < quarterl; i++)
+  {
+    // cos((N/2 + k) * theta) = -cos((N/2 - k) * theta)
+    // sin((N/2 + k) * theta) = -sin((N/2 - k) * theta)
+    twiddle_factors->real[halfl + i] = twiddle_factors->real[halfl - i];
+    twiddle_factors->imag[halfl + i] = -1 * twiddle_factors->imag[halfl - i];
+  }
+}
 
 complex* FFTLIBRARY_CALL precompute_twiddle_factor(int length, int backward)
 {
@@ -13,20 +72,11 @@ complex* FFTLIBRARY_CALL precompute_twiddle_factor(int length, int backward)
     due to the nature of fft, we only need N/2 point of the twiddle factor
   */
 
-  double theta;
-
-  double magic_factor_1;
-  double magic_factor_2;
-
   int half_length;
   int N_over_eight;
   int N_over_four;
 
   complex* twiddle_factors;
-
-  theta = backward ? (M_PI * 2) / length : -(M_PI * 2) / length;
-  magic_factor_1 = 1 - 2 * pow(sin(theta / 2), 2); // C
-  magic_factor_2 = sin(theta);                     // S
 
   half_length = length >> 1;
   N_over_four = half_length >> 1;
@@ -49,39 +99,12 @@ complex* FFTLIBRARY_CALL precompute_twiddle_factor(int length, int backward)
     break;
 
   default:
-    for (int i = 1; i <= N_over_eight; i++)
-    {
-      // using singleteon
-      // cos ((r + 1)theta) = C * cos(rtheta) - S * sin(rtheta)
-      // sin((r + 1)theta) = S * cos(rtheta) + C * sin(rtheta)
-
-      twiddle_factors->real[i] = magic_factor_1 * twiddle_factors->real[i - 1] - magic_factor_2 * twiddle_factors->imag[i - 1];
-      twiddle_factors->imag[i] = magic_factor_2 * twiddle_factors->real[i - 1] + magic_factor_1 * twiddle_factors->imag[i - 1];
-    }
-
-    for (int i = 1; i < N_over_eight; i++)
-    {
-      // cos((N/8 + k) * theta) = sin((N/8 - k) * theta)
-      // sin((N/8 + k) * theta) = cos((N/8 - k) * theta)
-
-      twiddle_factors->real[N_over_eight + i] = backward ? twiddle_factors->imag[N_over_eight - i] : -twiddle_factors->imag[N_over_eight - i];
-      twiddle_factors->imag[N_over_eight + i] = backward ? twiddle_factors->real[N_over_eight - i] : -twiddle_factors->real[N_over_eight - i];
-    }
-
-    // calculate the other N / 4 points in second quadrand
-    for (int i = 0; i < N_over_four; i++)
-    {
-      // cos((N/4 + k) * theta) = -sin(k * theta)
-      // sin((N/4 + k) * theta) = cos(k * theta)
-
-      twiddle_factors->real[N_over_four + i] = backward ? -twiddle_factors->imag[i] : twiddle_factors->imag[i];
-      twiddle_factors->imag[N_over_four + i] = backward ? twiddle_factors->real[i] : -twiddle_factors->real[i];
-    }
+    twid_calc_routine(twiddle_factors, length, half_length, N_over_four, N_over_eight, backward);
   }
   return twiddle_factors;
 }
 
-complex* FFTLIBRARY_CALL precompute_twiddle_factor_radix_4(int length, int backward)
+complex** FFTLIBRARY_CALL precompute_twiddle_factor_radix_4(int length, int backward)
 {
   /*
     Twiddle factor is symmetric
@@ -92,81 +115,52 @@ complex* FFTLIBRARY_CALL precompute_twiddle_factor_radix_4(int length, int backw
     due to the nature of fft, we only need N/2 point of the twiddle factor
   */
 
-  double theta;
-
-  double magic_factor_1;
-  double magic_factor_2;
-
   int half_length;
   int N_over_eight;
   int N_over_four;
 
   complex* twiddle_factors;
-
-  theta = backward ? (M_PI * 2) / length : -(M_PI * 2) / length;
-  magic_factor_1 = 1 - 2 * pow(sin(theta / 2), 2); // C
-  magic_factor_2 = sin(theta);                     // S
+  complex* twid_factors_2l;
+  complex* twid_factors_3l;
 
   half_length = length >> 1;
   N_over_four = half_length >> 1;
   N_over_eight = N_over_four >> 1;
 
   twiddle_factors = complex_arr_create_allign_16(3 * N_over_four);
+  twid_factors_2l = malloc(sizeof( complex ));
+  twid_factors_3l = malloc(sizeof( complex ));
 
   twiddle_factors->real[0] = 1;
   twiddle_factors->imag[0] = 0;
 
   switch (length)
   {
-  case 2:
-    break;
+    case 4:
+      // twiddle_factors[1] = backward ? create_complex_number(0, 1) : create_complex_number(0, -1);
+      twiddle_factors->real[1] = 0;
+      twiddle_factors->imag[1] = backward ? 1 : -1;
+      break;
 
-  case 4:
-    // twiddle_factors[1] = backward ? create_complex_number(0, 1) : create_complex_number(0, -1);
-    twiddle_factors->real[1] = 0;
-    twiddle_factors->imag[1] = backward ? 1 : -1;
-    break;
+    default:
+      twid_calc_routine_r4(twiddle_factors, length, half_length, N_over_four, N_over_eight, backward);
 
-  default:
-    for (int i = 1; i <= N_over_eight; i++)
-    {
-      // using singleteon
-      // cos ((r + 1)theta) = C * cos(rtheta) - S * sin(rtheta)
-      // sin((r + 1)theta) = S * cos(rtheta) + C * sin(rtheta)
+      // remove unused twid factors
+      // l = [0, N/4) -> N/4
+      // 2l = [0, 2, 4, ..., N/2) -> N/4
+      // 3l = [0, 3, 6, ..., 3N/4) -> N/4
 
-      twiddle_factors->real[i] = magic_factor_1 * twiddle_factors->real[i - 1] - magic_factor_2 * twiddle_factors->imag[i - 1];
-      twiddle_factors->imag[i] = magic_factor_2 * twiddle_factors->real[i - 1] + magic_factor_1 * twiddle_factors->imag[i - 1];
-    }
+      for (int i = 0; i < N_over_four; i++)
+      {
+        twiddle_factors->real[half_length + i] = twiddle_factors->real[3 * i];
+        twiddle_factors->imag[half_length + i] = twiddle_factors->imag[3 * i];
+      }
 
-    for (int i = 1; i < N_over_eight; i++)
-    {
-      // cos((N/8 + k) * theta) = sin((N/8 - k) * theta)
-      // sin((N/8 + k) * theta) = cos((N/8 - k) * theta)
-
-      twiddle_factors->real[N_over_eight + i] = backward ? twiddle_factors->imag[N_over_eight - i] : -twiddle_factors->imag[N_over_eight - i];
-      twiddle_factors->imag[N_over_eight + i] = backward ? twiddle_factors->real[N_over_eight - i] : -twiddle_factors->real[N_over_eight - i];
-    }
-
-    // calculate the other N / 4 points in second quadrand
-    for (int i = 0; i <= N_over_four; i++)
-    {
-      // cos((N/4 + k) * theta) = -sin(k * theta)
-      // sin((N/4 + k) * theta) = cos(k * theta)
-
-      twiddle_factors->real[N_over_four + i] = backward ? -twiddle_factors->imag[i] : twiddle_factors->imag[i];
-      twiddle_factors->imag[N_over_four + i] = backward ? twiddle_factors->real[i] : -twiddle_factors->real[i];
-    }
-
-    // Extra N/4 
-    // calculate the other N / 4 points in third quadrand
-    for (int i = 1; i < N_over_four; i++)
-    {
-      // cos((N/2 + k) * theta) = -cos((N/2 - k) * theta)
-      // sin((N/2 + k) * theta) = -sin((N/2 - k) * theta)
-
-      twiddle_factors->real[half_length + i] = twiddle_factors->real[half_length - i];
-      twiddle_factors->imag[half_length + i] = -1 * twiddle_factors->imag[half_length - i];
-    }
+      for (int i = 0; i < N_over_four; i++)
+      {
+        twiddle_factors->real[N_over_four + i] = twiddle_factors->real[2 * i];
+        twiddle_factors->imag[N_over_four + i] = twiddle_factors->imag[2 * i];
+      }
   }
   return twiddle_factors;
 }
