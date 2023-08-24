07cc7767613b69b8deb1fd71f721cd041b6168e1
AdityaP1502 <i.m.aditya015@gmail.com>
2023-08-24 23:34:24 +0700

    Refactor code and add butterfly for ifft

--- header/simd/fft_br4.h
+++ header/simd/fft_br4.h
@@ -11,17 +11,19 @@ void fft_br4_f(complex* c_arr, complex* W, int j0, int k, int ql, int hl);
 
 void fft_br4_b(complex* c_arr, complex* W, int j0, int k, int ql, int hl);
 
-void fft_br4_0(complex* c_arr, int j0, int ql, int hl);
+void fft_br4_f_0(complex* c_arr, int j0, int ql, int hl);
 
-void fft_br4_f_N_over_8(complex* c_arr, int j0, int ql, int hl);
+void fft_br4_b_0(complex* c_arr, int j0, int ql, int hl);
 
-void fft_br4_b_N_over_16(complex* c_arr, int j0, int ql, int hl);
+void fft_br4_f_N_over_8(complex* c_arr, complex* W, int j0, int ql, int hl, int k);
 
 void fft_br4_f_N_over_16(complex* c_arr, complex* W, int j0, int ql, int hl, int k);
 
+void fft_br4_f_3N_over_16(complex* c_arr, complex* W, int j0, int ql, int hl, int k);
+
 void fft_br4_b_N_over_8(complex* c_arr, complex* W, int j0, int ql, int hl, int k);
 
-void fft_br4_f_3N_over_16(complex* c_arr, complex* W, int j0, int ql, int hl, int k);
+void fft_br4_b_N_over_16(complex* c_arr, complex* W, int j0, int ql, int hl, int k);
 
 void fft_br4_b_3N_over_16(complex* c_arr, complex* W, int j0, int ql, int hl, int k);
 
